ecb006d084c435a3b1af287bbf5c84d0ecaecf3d
chaeri93 <chaeri93@ewhain.net>
2021-11-21 01:34:17 +0900

    <layout> 달성률 상단 탭 레이아웃

--- src/screens/AchievementScreen.js
+++ src/screens/AchievementScreen.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-class AchievementScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Achievement</Text>
-      </View>
-    );
-  }
+import {SafeAreaView} from 'react-native';
+import AppNavigator from '../Navigator/AchieveNavigator';
+
+export default function () {
+  return (
+      <>
+        <SafeAreaView />
+        <AppNavigator />
+      </>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
-
-export default AchievementScreen;
