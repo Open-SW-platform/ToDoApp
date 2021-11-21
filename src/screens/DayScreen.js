11419d3da2ed81e119bbc881a9868b3a16cd08e8
chaeri93 <chaeri93@ewhain.net>
2021-11-22 00:16:46 +0900

    <chore> Dayscreen 수정

--- src/screens/DayScreen.js
+++ src/screens/DayScreen.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import { images } from '../images';
+import IconButton from '../components/IconButton';
+import {viewStyles, textStyles, barStyles} from '../styles'
 
-class DayScreen extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>DayScreen</Text>
-            </View>
-        );
-    }
+
+
+export default function DayScreen() {
+    return (
+        <View style={viewStyles.settingBar}>
+            <Text>  8.30 Monday </Text>
+            <IconButton type={images.dropDown} />
+        </View>
+
+    );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-});
 
-export default DayScreen;
